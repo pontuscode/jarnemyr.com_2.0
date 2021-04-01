84217f6457f3e3862dfd05da58a13601e94e2888
Pontus Jarnemyr <pontus@jarnemyr.com>
2021-04-01 16:13:02 +0200

    Website skeleton is now complete. Need to add content for project sub-pages.

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__)
-MEDIA_FOLDER = "/home/pontus/flask_webserver_2.0/static/tutorial_stuff/img"
 
 
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 	
+	
 @app.route('/projects', methods=['GET'])
 def projects():
 	return render_template('projects.html')
 	
+	
 @app.route('/contact', methods=['GET'])
 def about():
 	return render_template('contact.html')
-#@app.route('/sayhello', methods=["POST"])
-#def sayhello():
-#	return "hey man xD";
 
+
+@app.route('/projects/arbitrage', methods=['GET'])
+def arbitrage():
+	return render_template('/projects/arbitrage.html')
+
+
+@app.route('/projects/lending-tracker', methods=['GET'])
+def lending_tracker():
+	return render_template('/projects/lending-tracker.html')
+	
+
+@app.route('/projects/portfolio-tracker', methods=['GET'])
+def portfolio_tracker():
+	return render_template('/projects/portfolio-tracker.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
